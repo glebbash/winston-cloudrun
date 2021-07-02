437e1dab3b7f11b14d99bbf9ee046246e304fbbb
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2021-07-02 09:47:49 +0000

    fix: coverage

--- src/winston-cloudrun.spec.ts
+++ src/winston-cloudrun.spec.ts
@@ -81,5 +81,38 @@ describe('winston-cloudrun-config', () => {
         [Symbol.for('message')]: transformed?.[Symbol.for('message') as never],
       });
     });
+
+    it('transforms input correctly (with tracing, traceSampled true by default)', () => {
+      const input: TransformableInfo = {
+        level: 'info',
+        message: 'hello there',
+      };
+
+      const dateSpy = jest.spyOn(Date.prototype, 'toISOString');
+
+      const traceId = 'traceId';
+      const spanId = 'spanId';
+      const format = getCloudLoggingFormat({
+        getTrace: () => ({
+          traceId,
+          spanId,
+        }),
+      });
+      const transformed = format?.transform(input);
+
+      expect(transformed).toEqual({
+        message: input.message,
+        severity: 'INFO',
+
+        'logging.googleapis.com/spanId': spanId,
+        'logging.googleapis.com/trace': traceId,
+        'logging.googleapis.com/trace_sampled': true,
+
+        time: dateSpy.mock.results[0].value, // current date in ISO format
+
+        // winston internal stringified version of data
+        [Symbol.for('message')]: transformed?.[Symbol.for('message') as never],
+      });
+    });
   });
 });
