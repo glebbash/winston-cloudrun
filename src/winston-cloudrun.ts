2814177c1c6dcfd395e62cb7bbc4355b95c059ce
yaroslavkrutiak <yaroslav.krut02@gmail.com>
2022-08-19 17:27:01 +0300

    feat: add tenantId and correlationId labels
    Signed-off-by: yaroslavkrutiak <yaroslav.krut02@gmail.com>

--- src/winston-cloudrun.ts
+++ src/winston-cloudrun.ts
@@ -2,12 +2,22 @@ import { Format } from 'logform';
 import { format, LoggerOptions, transports } from 'winston';
 
 export type GetTraceFn = () => { traceId: string; spanId: string; traceSampled?: boolean };
+export type GetTenantIdFn = () => string;
+export type GetCorrelationIdFn = () => string;
 export type WinstonCloudRunConfig = { production: boolean; getTrace?: GetTraceFn };
 
 /**
  * Creates Winston format that specifies time and renames level to severity
  */
-export function getCloudLoggingFormat({ getTrace }: { getTrace?: GetTraceFn } = {}): Format {
+export function getCloudLoggingFormat({
+  getTrace,
+  getTenantIdFn,
+  getCorrelationIdFn,
+}: {
+  getTrace?: GetTraceFn;
+  getTenantIdFn?: GetTenantIdFn;
+  getCorrelationIdFn?: GetCorrelationIdFn;
+} = {}): Format {
   return format.combine(
     format.errors({ stack: true }),
     format((info) => {
@@ -16,6 +26,12 @@ export function getCloudLoggingFormat({ getTrace }: { getTrace?: GetTraceFn } = 
       return {
         ...data,
         ...(getTrace && { ...getTraceInfo(getTrace) }),
+        ...(getCorrelationIdFn && {
+          'logging.googleapis.com/labels': {
+            ...getCorrelationId(getCorrelationIdFn),
+            ...(getTenantIdFn && { ...getTenantId(getTenantIdFn) }),
+          },
+        }),
         severity: level.toUpperCase(),
         time: new Date().toISOString(),
       } as never;
@@ -47,5 +63,17 @@ function getTraceInfo(getTrace: GetTraceFn) {
     'logging.googleapis.com/trace': traceId,
     'logging.googleapis.com/spanId': spanId,
     'logging.googleapis.com/trace_sampled': traceSampled,
+  };
+}
+
+export function getCorrelationId(getCorrelationIdFn: GetCorrelationIdFn) {
+  return {
+    'Correlation-Id': getCorrelationIdFn(),
+  };
+}
+
+export function getTenantId(getTenantIdFn: GetTenantIdFn) {
+  return {
+    'Tenant-Id': getTenantIdFn(),
   };
 }
